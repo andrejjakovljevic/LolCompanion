6e38c52eab72804e33bbe25e1ae008c45a9c4df1
gavantee <milovancevicdragan@gmx.com>
2021-04-17 13:45:51 +0200

    header 3. put

--- LolCompanion_v0.1/app/Views/template/header.php
+++ LolCompanion_v0.1/app/Views/template/header.php
@@ -5,26 +5,25 @@
         <link rel="stylesheet" href="stil.css">
         <link href="https://fonts.googleapis.com/css2?family=Syne+Mono&display=swap" rel="stylesheet">
         <title>LolCompanion</title>
-        <link rel="shortcut icon" type="image/png" href="slike/lolporo.png">
     </head>
-    <div class="container-fluid font">
-        <div class="row header">
-            <body>
-                <div class="container-fluid font">
-            <div class="col-lg-3">
-                <img src="slike/lolporo.png" style="width: 20%;">
-                <a href="index.html" class="basic">LoLCompanion</a>
+    <body>
+        <div class="container-fluid font">
+            <div class="row header">
+                    <div class="container-fluid font">
+                <div class="col-lg-3">
+                    <img src="slike/lolporo.png" style="width: 20%;">
+                    <a href="index.html" class="basic">LoLCompanion</a>
+                </div>
+                <div class="col-lg-6 header">
+                    &nbsp;
+                </div>
+                <div class="col-lg-1 align-self-center">
+                    <a href="champions.html" class="basic">Champions</a>  
+                </div>
+                <div class="col-lg-1">
+                    &nbsp;
+                </div>
+                <div class="col-lg-1 align-self-center">
+                    <a href="login.html" class="basic">Login</a>
+                </div>
             </div>
-            <div class="col-lg-6 header">
-                &nbsp;
-            </div>
-            <div class="col-lg-1 align-self-center">
-                <a href="champions.html" class="basic">Champions</a>  
-            </div>
-            <div class="col-lg-1">
-                &nbsp;
-            </div>
-            <div class="col-lg-1 align-self-center">
-                <a href="login.html" class="basic">Login</a>
-            </div>
-        </div>
