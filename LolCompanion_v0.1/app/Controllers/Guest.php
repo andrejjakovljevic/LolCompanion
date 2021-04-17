91705e02e08705dc2d9c4f5489aa4c8b683e5623
gavantee <milovancevicdragan@gmx.com>
2021-04-17 15:19:56 +0200

    login

--- LolCompanion_v0.1/app/Controllers/Guest.php
+++ LolCompanion_v0.1/app/Controllers/Guest.php
@@ -7,6 +7,25 @@ class Guest extends BaseController
 	public function index()
 	{
 		echo view('template/header.php');
-                echo view('template/footer.php');
+		echo view('pages/login.php');
+        echo view('template/footer.php');
 	}
+
+    public function loginSubmit(){
+/*
+        if(!$this->validate(['korime'=>'required', 'lozinka'=>'required'])){
+            return $this->prikaz('login', 
+                ['errors'=>$this->validator->getErrors()]);
+        }
+		*/
+        $autorModel=new AutorModel();
+        $autor=$autorModel->find($this->request->getVar('korime'));
+        if($autor==null)
+            return $this->login('Korisnik ne postoji');
+        if($autor->lozinka!=$this->request->getVar('lozinka'))
+            return $this->login('Pogresna lozinka');
+        
+        $this->session->set('autor', $autor);
+        return redirect()->to(site_url('Korisnik'));
+    }
 }
