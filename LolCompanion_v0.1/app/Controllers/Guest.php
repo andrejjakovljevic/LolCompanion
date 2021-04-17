b53825f20ce7f93bd7b54dd361f371c91feca31b
Andrej Jakovljevic <andrej.jakovljevic2000@gmail.com>
2021-04-17 13:50:31 +0200

    commit

--- LolCompanion_v0.1/app/Controllers/Guest.php
+++ LolCompanion_v0.1/app/Controllers/Guest.php
@@ -7,5 +7,6 @@ class Guest extends BaseController
 	public function index()
 	{
 		echo view('template/header.php');
+                echo view('template/footer.php');
 	}
 }
--- LolCompanion_v0.1/app/Views/template/header.php
+++ LolCompanion_v0.1/app/Views/template/header.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <!--Autor: Dragan Milovancevic 18/0153-->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="stil.css">
         <link href="https://fonts.googleapis.com/css2?family=Syne+Mono&display=swap" rel="stylesheet">
         <title>LolCompanion</title>
