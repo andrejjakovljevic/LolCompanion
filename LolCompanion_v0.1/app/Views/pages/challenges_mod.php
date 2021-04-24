e89f5ccbb23204019f195f3d8a43e7a9d6f85fd8
veljkozz <r.weljko@gmail.com>
2021-04-24 15:12:06 +0200

    Create challenges_mod.php

---
+++ LolCompanion_v0.1/app/Views/pages/challenges_mod.php
@@ -0,0 +1,55 @@
+<div class = "row">
+                <div class = "col-lg-12 text-center naslov" style="padding: 0; font-weight: normal;">
+                    All Challenges
+                </div>
+                <br>
+                
+            </div>
+            <div class = "row">
+                <div class="col-lg-12">
+                    <a href="#"><button class="center" style="width: 200px"><span style="font-weight: bold;">New Challenge</span></button></a>
+                    <img src= <?= base_url("slike/Line.png")?> class="center" height="20px">
+                </div>
+            </div>
+            <div class="container" style="background-color: rgba(100, 149, 237, 0.5); padding-bottom: 10px; border: 1pt solid darkred;">
+                <div class = "row">
+                    <div class = "col-lg-9 col-md-6 col-xs-12" >
+                        The First Quest
+                        <br>
+                        <img src="slike/dummyQuestPic.jpg" style="height: 60px;float: left; padding-right: 10px; padding-top: 2px;">
+                        <span>The first quest is a very good quest. Play Xin top.</span> 
+                    </div>
+                    <div class="col-lg-2 col-md-5 col-xs-10" style="overflow-y: auto;">
+                        Champion: Xin Zhao<br>
+                        Role: Top<br>
+                        Kills: 1<br>
+                    </div>
+                    <div class="col-lg-1 col-md-1 col-xs-2">
+                        <div>
+                            <a href=""><img src="slike/trash.png" style="height: 50px; margin-top: 10px"></img></a>
+                        </div>
+                        <div>Id: 1</div>
+                    </div>
+                </div>
+                <hr style="margin: 5px;">
+                <div class = "row">
+                    <div class = "col-lg-9 col-md-6 col-xs-12" >
+                        The Second Quest
+                        <br>
+                        <img src="slike/dummyQuestPic.jpg" style="height: 60px;float: left; padding-right: 10px; padding-top: 2px;">
+                        <span>Valhala awaits you brave warrior.</span> 
+                    </div>
+                    <div class="col-lg-2 col-md-5 col-xs-10" style="overflow-y: auto;">
+                        Champion: Olaf<br>
+                        Role: Jungle<br>
+                        Kills: 100<br>
+                    </div>
+                    <div class="col-lg-1 col-md-1 col-xs-2">
+                        <div>
+                            <a href=""><img src="slike/trash.png" style="height: 50px; margin-top: 10px"></img></a>
+                        </div>
+                        <div>Id: 2</div>
+                    </div>
+                </div>
+            </div>
+            
