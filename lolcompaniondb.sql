8f2817852f5bd840438459f8deac95653281c0a6
Andrej Jakovljevic <andrej.jakovljevic2000@gmail.com>
2021-04-24 15:01:27 +0200

    Update lolcompaniondb.sql

--- lolcompaniondb.sql
+++ lolcompaniondb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 24, 2021 at 12:00 PM
+-- Generation Time: Apr 24, 2021 at 01:00 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -31,18 +31,20 @@ USE `lolcompaniondb`;
 
 DROP TABLE IF EXISTS `builds`;
 CREATE TABLE IF NOT EXISTS `builds` (
-  `iditem1` int(11) NOT NULL,
+  `iditem1` int(11) DEFAULT NULL,
   `iditem2` int(11) DEFAULT NULL,
   `iditem3` int(11) DEFAULT NULL,
-  `idhchamp` int(11) DEFAULT NULL,
-  `winrate` double DEFAULT NULL,
-  `pickrate` double DEFAULT NULL,
-  `lane` varchar(45) DEFAULT NULL,
+  `idhchamp` int(11) NOT NULL,
+  `winrate` double NOT NULL,
+  `pickrate` double NOT NULL,
+  `lane` varchar(45) NOT NULL,
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `iditems_idx` (`iditem1`),
   KEY `fk2_iditems_idx` (`iditem2`),
   KEY `fk3_iditems_idx` (`iditem3`),
   KEY `fk_idchamps_idx` (`idhchamp`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -55,168 +57,168 @@ CREATE TABLE IF NOT EXISTS `champions` (
   `id` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `champions`
 --
 
 INSERT INTO `champions` (`id`, `name`) VALUES
-(266, 'Aatrox'),
-(103, 'Ahri'),
-(84, 'Akali'),
-(12, 'Alistar'),
-(32, 'Amumu'),
-(34, 'Anivia'),
 (1, 'Annie'),
-(523, 'Aphelios'),
-(22, 'Ashe'),
-(136, 'Aurelion Sol'),
-(268, 'Azir'),
-(432, 'Bard'),
-(53, 'Blitzcrank'),
-(63, 'Brand'),
-(201, 'Braum'),
-(51, 'Caitlyn'),
-(164, 'Camille'),
-(69, 'Cassiopeia'),
-(31, 'Cho\'Gath'),
-(42, 'Corki'),
-(122, 'Darius'),
-(131, 'Diana'),
-(119, 'Draven'),
-(36, 'Dr. Mundo'),
-(245, 'Ekko'),
-(60, 'Elise'),
-(28, 'Evelynn'),
-(81, 'Ezreal'),
-(9, 'Fiddlesticks'),
-(114, 'Fiora'),
-(105, 'Fizz'),
-(3, 'Galio'),
-(41, 'Gangplank'),
-(86, 'Garen'),
-(150, 'Gnar'),
-(79, 'Gragas'),
-(104, 'Graves'),
-(887, 'Gwen'),
-(120, 'Hecarim'),
-(74, 'Heimerdinger'),
-(420, 'Illaoi'),
-(39, 'Irelia'),
-(427, 'Ivern'),
-(40, 'Janna'),
-(59, 'Jarvan IV'),
-(24, 'Jax'),
-(126, 'Jayce'),
-(202, 'Jhin'),
-(222, 'Jinx'),
-(145, 'Kai\'Sa'),
-(429, 'Kalista'),
-(43, 'Karma'),
-(30, 'Karthus'),
-(38, 'Kassadin'),
-(55, 'Katarina'),
-(10, 'Kayle'),
-(141, 'Kayn'),
-(85, 'Kennen'),
-(121, 'Kha\'Zix'),
-(203, 'Kindred'),
-(240, 'Kled'),
-(96, 'Kog\'Maw'),
-(7, 'LeBlanc'),
-(64, 'Lee Sin'),
-(89, 'Leona'),
-(876, 'Lillia'),
-(127, 'Lissandra'),
-(236, 'Lucian'),
-(117, 'Lulu'),
-(99, 'Lux'),
-(54, 'Malphite'),
-(90, 'Malzahar'),
-(57, 'Maokai'),
-(11, 'Master Yi'),
-(21, 'Miss Fortune'),
-(62, 'Wukong'),
-(82, 'Mordekaiser'),
-(25, 'Morgana'),
-(267, 'Nami'),
-(75, 'Nasus'),
-(111, 'Nautilus'),
-(518, 'Neeko'),
-(76, 'Nidalee'),
-(56, 'Nocturne'),
-(20, 'Nunu & Willump'),
 (2, 'Olaf'),
-(61, 'Orianna'),
-(516, 'Ornn'),
-(80, 'Pantheon'),
-(78, 'Poppy'),
-(555, 'Pyke'),
-(246, 'Qiyana'),
-(133, 'Quinn'),
-(497, 'Rakan'),
-(33, 'Rammus'),
-(421, 'Rek\'Sai'),
-(526, 'Rell'),
-(58, 'Renekton'),
-(107, 'Rengar'),
-(92, 'Riven'),
-(68, 'Rumble'),
+(3, 'Galio'),
+(4, 'Twisted Fate'),
+(5, 'Xin Zhao'),
+(6, 'Urgot'),
+(7, 'LeBlanc'),
+(8, 'Vladimir'),
+(9, 'Fiddlesticks'),
+(10, 'Kayle'),
+(11, 'Master Yi'),
+(12, 'Alistar'),
 (13, 'Ryze'),
-(360, 'Samira'),
-(113, 'Sejuani'),
-(235, 'Senna'),
-(147, 'Seraphine'),
-(875, 'Sett'),
-(35, 'Shaco'),
-(98, 'Shen'),
-(102, 'Shyvana'),
-(27, 'Singed'),
 (14, 'Sion'),
 (15, 'Sivir'),
-(72, 'Skarner'),
-(37, 'Sona'),
 (16, 'Soraka'),
-(50, 'Swain'),
-(517, 'Sylas'),
-(134, 'Syndra'),
-(223, 'Tahm Kench'),
-(163, 'Taliyah'),
-(91, 'Talon'),
-(44, 'Taric'),
 (17, 'Teemo'),
-(412, 'Thresh'),
 (18, 'Tristana'),
-(48, 'Trundle'),
-(23, 'Tryndamere'),
-(4, 'Twisted Fate'),
-(29, 'Twitch'),
-(77, 'Udyr'),
-(6, 'Urgot'),
-(110, 'Varus'),
-(67, 'Vayne'),
-(45, 'Veigar'),
-(161, 'Vel\'Koz'),
-(254, 'Vi'),
-(234, 'Viego'),
-(112, 'Viktor'),
-(8, 'Vladimir'),
-(106, 'Volibear'),
 (19, 'Warwick'),
-(498, 'Xayah'),
-(101, 'Xerath'),
-(5, 'Xin Zhao'),
-(157, 'Yasuo'),
-(777, 'Yone'),
-(83, 'Yorick'),
-(350, 'Yuumi'),
-(154, 'Zac'),
-(238, 'Zed'),
-(115, 'Ziggs'),
+(20, 'Nunu & Willump'),
+(21, 'Miss Fortune'),
+(22, 'Ashe'),
+(23, 'Tryndamere'),
+(24, 'Jax'),
+(25, 'Morgana'),
 (26, 'Zilean'),
+(27, 'Singed'),
+(28, 'Evelynn'),
+(29, 'Twitch'),
+(30, 'Karthus'),
+(31, 'Cho\'Gath'),
+(32, 'Amumu'),
+(33, 'Rammus'),
+(34, 'Anivia'),
+(35, 'Shaco'),
+(36, 'Dr. Mundo'),
+(37, 'Sona'),
+(38, 'Kassadin'),
+(39, 'Irelia'),
+(40, 'Janna'),
+(41, 'Gangplank'),
+(42, 'Corki'),
+(43, 'Karma'),
+(44, 'Taric'),
+(45, 'Veigar'),
+(48, 'Trundle'),
+(50, 'Swain'),
+(51, 'Caitlyn'),
+(53, 'Blitzcrank'),
+(54, 'Malphite'),
+(55, 'Katarina'),
+(56, 'Nocturne'),
+(57, 'Maokai'),
+(58, 'Renekton'),
+(59, 'Jarvan IV'),
+(60, 'Elise'),
+(61, 'Orianna'),
+(62, 'Wukong'),
+(63, 'Brand'),
+(64, 'Lee Sin'),
+(67, 'Vayne'),
+(68, 'Rumble'),
+(69, 'Cassiopeia'),
+(72, 'Skarner'),
+(74, 'Heimerdinger'),
+(75, 'Nasus'),
+(76, 'Nidalee'),
+(77, 'Udyr'),
+(78, 'Poppy'),
+(79, 'Gragas'),
+(80, 'Pantheon'),
+(81, 'Ezreal'),
+(82, 'Mordekaiser'),
+(83, 'Yorick'),
+(84, 'Akali'),
+(85, 'Kennen'),
+(86, 'Garen'),
+(89, 'Leona'),
+(90, 'Malzahar'),
+(91, 'Talon'),
+(92, 'Riven'),
+(96, 'Kog\'Maw'),
+(98, 'Shen'),
+(99, 'Lux'),
+(101, 'Xerath'),
+(102, 'Shyvana'),
+(103, 'Ahri'),
+(104, 'Graves'),
+(105, 'Fizz'),
+(106, 'Volibear'),
+(107, 'Rengar'),
+(110, 'Varus'),
+(111, 'Nautilus'),
+(112, 'Viktor'),
+(113, 'Sejuani'),
+(114, 'Fiora'),
+(115, 'Ziggs'),
+(117, 'Lulu'),
+(119, 'Draven'),
+(120, 'Hecarim'),
+(121, 'Kha\'Zix'),
+(122, 'Darius'),
+(126, 'Jayce'),
+(127, 'Lissandra'),
+(131, 'Diana'),
+(133, 'Quinn'),
+(134, 'Syndra'),
+(136, 'Aurelion Sol'),
+(141, 'Kayn'),
 (142, 'Zoe'),
-(143, 'Zyra');
+(143, 'Zyra'),
+(145, 'Kai\'Sa'),
+(147, 'Seraphine'),
+(150, 'Gnar'),
+(154, 'Zac'),
+(157, 'Yasuo'),
+(161, 'Vel\'Koz'),
+(163, 'Taliyah'),
+(164, 'Camille'),
+(201, 'Braum'),
+(202, 'Jhin'),
+(203, 'Kindred'),
+(222, 'Jinx'),
+(223, 'Tahm Kench'),
+(234, 'Viego'),
+(235, 'Senna'),
+(236, 'Lucian'),
+(238, 'Zed'),
+(240, 'Kled'),
+(245, 'Ekko'),
+(246, 'Qiyana'),
+(254, 'Vi'),
+(266, 'Aatrox'),
+(267, 'Nami'),
+(268, 'Azir'),
+(350, 'Yuumi'),
+(360, 'Samira'),
+(412, 'Thresh'),
+(420, 'Illaoi'),
+(421, 'Rek\'Sai'),
+(427, 'Ivern'),
+(429, 'Kalista'),
+(432, 'Bard'),
+(497, 'Rakan'),
+(498, 'Xayah'),
+(516, 'Ornn'),
+(517, 'Sylas'),
+(518, 'Neeko'),
+(523, 'Aphelios'),
+(526, 'Rell'),
+(555, 'Pyke'),
+(777, 'Yone'),
+(875, 'Sett'),
+(876, 'Lillia'),
+(887, 'Gwen');
 
 -- --------------------------------------------------------
 
@@ -229,7 +231,7 @@ CREATE TABLE IF NOT EXISTS `global` (
   `name` varchar(30) NOT NULL,
   `value` varchar(100) NOT NULL,
   PRIMARY KEY (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `global`
@@ -249,7 +251,7 @@ CREATE TABLE IF NOT EXISTS `items` (
   `id` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -264,7 +266,7 @@ CREATE TABLE IF NOT EXISTS `korisnik` (
   `email` varchar(50) NOT NULL,
   `role` int(11) NOT NULL,
   PRIMARY KEY (`summonerName`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `korisnik`
@@ -290,7 +292,7 @@ CREATE TABLE IF NOT EXISTS `plays` (
   `role` int(11) DEFAULT NULL,
   KEY `fk_korisnik_idx` (`summonername`),
   KEY `fk_champion_idx` (`idchamp`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -305,7 +307,7 @@ CREATE TABLE IF NOT EXISTS `quest` (
   `title` varchar(50) NOT NULL,
   `image` varchar(300) NOT NULL,
   PRIMARY KEY (`questId`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `quest`
@@ -326,8 +328,9 @@ CREATE TABLE IF NOT EXISTS `questattributes` (
   `questId` int(11) NOT NULL,
   `attributeKey` varchar(50) NOT NULL,
   `attributeValue` varchar(50) NOT NULL,
-  PRIMARY KEY (`attributeId`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`attributeId`),
+  KEY `fk_quest_idx` (`questId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `questattributes`
@@ -348,9 +351,11 @@ DROP TABLE IF EXISTS `userquest`;
 CREATE TABLE IF NOT EXISTS `userquest` (
   `summonerName` varchar(40) NOT NULL,
   `questId` int(11) NOT NULL,
-  `completed` tinyint(1) NOT NULL,
-  PRIMARY KEY (`questId`,`summonerName`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `completed` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`summonerName`,`questId`),
+  KEY `fk_summoner_idx` (`summonerName`),
+  KEY `fk_quest_idx` (`questId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `userquest`
@@ -358,6 +363,39 @@ CREATE TABLE IF NOT EXISTS `userquest` (
 
 INSERT INTO `userquest` (`summonerName`, `questId`, `completed`) VALUES
 ('GINDRA', 0, 0);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `builds`
+--
+ALTER TABLE `builds`
+  ADD CONSTRAINT `fk_champion` FOREIGN KEY (`idhchamp`) REFERENCES `champions` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_item1` FOREIGN KEY (`iditem1`) REFERENCES `items` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_item2` FOREIGN KEY (`iditem2`) REFERENCES `items` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_item3` FOREIGN KEY (`iditem3`) REFERENCES `items` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `plays`
+--
+ALTER TABLE `plays`
+  ADD CONSTRAINT `fk_champ` FOREIGN KEY (`idchamp`) REFERENCES `champions` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_summoner` FOREIGN KEY (`summonername`) REFERENCES `korisnik` (`summonerName`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `questattributes`
+--
+ALTER TABLE `questattributes`
+  ADD CONSTRAINT `fk_quest` FOREIGN KEY (`questId`) REFERENCES `quest` (`questId`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `userquest`
+--
+ALTER TABLE `userquest`
+  ADD CONSTRAINT `fk_questquest` FOREIGN KEY (`questId`) REFERENCES `quest` (`questId`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_sumquest` FOREIGN KEY (`summonerName`) REFERENCES `korisnik` (`summonerName`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
