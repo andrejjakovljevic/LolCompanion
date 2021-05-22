849b77918461613c201b0bf8bc7314286226e568
Andrej Jakovljevic <andrej.jakovljevic2000@gmail.com>
2021-05-22 16:20:25 +0200

    Create error_page.php

---
+++ LolCompanion_v0.1/app/Views/pages/error_page.php
@@ -0,0 +1,11 @@
+
+<div class="row">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center" style="color: red">
+                        <h1>Oops, you have reached the limit of calls, please try again in 2 minutes.</h1>
+                    </div>
+                </div> 
+            </div>
+        </div>
+
